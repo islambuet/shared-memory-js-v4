65f9945d56e41f85287fc267cb2192bbcfa6870a
Md. Shaiful Islam <islambuet@gmail.com>
2023-03-11 01:24:31 +0600

    Read structure completed

---
+++ array_01.js
@@ -0,0 +1,50 @@
+let array_01_value=  {
+    ulong_msCounter: 54348785,
+    ulong_SorterEncoder: 961088068,
+    ulong_SorterEncoder2: 961076097,
+    ulong_SorterEncoder3: 961088034,
+    uint_PLCStatus: 0,
+    uint_Data: [ 0, 0, 0, 21845 ],
+    uint_BarcodeCounter: 48315,
+    uint_BarcodeMsgLength: 18,
+    byte_BarcodeMsg: [
+        60, 83, 84, 102, 122, 104, 109, 103, 66, 57, 75, 95,
+        48, 48, 49,  95, 118,  62,   0,   0,  0,  0,  0,  0,
+        0,  0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,
+        0,  0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,
+        0,  0,  0,   0,   0,   0,   0,   0,  0,  0,  0,  0,
+        0,  0,  0,   0
+    ],
+    uint_DestinationCounter: 43898,
+    uint_DestinationSequentialNumber: 1879,
+    uint_DestinationCounterLength: 5,
+    uint_Spare1: 0,
+    byte_Destination: [
+        83, 48, 50, 48,
+        57, 49,  0,  0
+    ],
+    uint_AlternateDestinationLength: 0,
+    uint_Spare2: 0,
+    byte_AlternateDestination: [
+        83, 48, 50, 49,
+        55,  0,  0,  0
+    ],
+    uint_ConfirmationCounter: 0,
+    uint_ConfirmationSequentialNumber: 0,
+    uint_ConfirmationDestination: 0,
+    uint_ConfirmationSortCode: 0,
+    uint_ScanNotificationReadCounter: 35599,
+    uint_IntraloxSortMsgReadCounter: 0,
+    uint_DivertNotificationReadCounter: 35594,
+    uint_Cad99AnalogMsgReadCounter: 9323,
+    uint_DeviceConnected: [ 151, 0, 0, 0 ],
+    uint_IntraloxStatusStatus: [ 0, 0, 0 ],
+    uint_ChuteStatus: [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0
+    ]
+}
+module.exports ={value:array_01_value};
--- examples/read_write_struct.js
+++ examples/read_write_struct.js
@@ -1,21 +1,11 @@
-var shared_memory = require("..");
+let shared_memory = require("..");
+const {value: array_01_value} = require("../array_01");
 try{
   let memory1=shared_memory.createMemory("MEMORY1");
   let memory2=shared_memory.createMemory("MEMORY2");  
-  let array_01_value=  {
-      ulong_msCounter: 34320540,
-      ulong_Encoder: 1,
-      ulong_Encoder2: 2,
-      ulong_Encoder3: 3,
-      ulong_PLCStatus: 4,
-      ulong_Data: [
-        5, 6, 7, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 8, 9, 10
-      ],
-      ulong_DeviceConnected: [ 0, 0 ]
-    }
+  let array_01_value=  require('../array_01.js').value;
+  for(let i=0;i<2;i++){
+    array_01_value.ulong_msCounter++;
     console.log("Press key for Write Array 01.")
     shared_memory.getch();
     shared_memory.writeArray01ToMemory(memory1,array_01_value);
@@ -23,12 +13,14 @@ try{
     shared_memory.getch();
     let array_02_value_current=shared_memory.readArray02FromMemory(memory2);
     console.log(array_02_value_current);
+  }
+
     console.log("Press key for Close memmory.");
-    shared_memory.getch();    
+    shared_memory.getch();
     shared_memory.closeMemory(memory1);
     shared_memory.closeMemory(memory2);
     console.log("Press key for End.");
-    shared_memory.getch();
+shared_memory.getch();
 }
 catch(e){
     console.log("catch: " + e)
